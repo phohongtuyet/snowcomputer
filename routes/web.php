45349c529ad3ed5511ba227e03bbd901b45f3b89
phohongtuyet <phohongtuyetse@gmail.com>
2022-03-07 15:34:52 +0700

    edit email verification notification

--- routes/web.php
+++ routes/web.php
@@ -98,22 +98,6 @@ Route::prefix('khach-hang')->group(function() {
 	Route::post('/hoso', [KhachHangController::class, 'postHoSo'])->name('khachhang.hoso');	
 });
 
-Route::get('/email/verify', function () {
-    return view('auth.verify');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
 // Trang quản trị
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function() {
 
@@ -298,3 +282,20 @@ Route::prefix('admin')->name('admin.')->middleware('auth')->group(function() {
 
 
 });
+
+// Email verification notification
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
