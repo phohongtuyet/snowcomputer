fe6e06e5c6b056865d8b82e5f9397c106781e1ce
phohongtuyet <phohongtuyetse@gmail.com>
2022-04-12 15:08:24 +0700

    edit nhap sanpham excel

--- app/Imports/SanPhamImport.php
+++ app/Imports/SanPhamImport.php
@@ -7,16 +7,21 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\WithValidation;
-
-class SanPhamImport implements ToModel, WithHeadingRow, WithValidation
+use Maatwebsite\Excel\Validators\Failure;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\SkipsErrors;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
+class SanPhamImport implements ToModel, WithValidation, WithHeadingRow, SkipsOnError
 {
-    
+    use Importable,SkipsErrors;
+
     public function model(array $row)
     {
         return new SanPham([
-            'loaisanpham_id' => $row['loai_san_pham'],
             'hangsanxuat_id' => $row['hang_san_xuat'],
             'noisanxuat_id' => $row['noi_san_xuat'],
+            'loaisanpham_id' => $row['loai_san_pham'],
             'tensanpham' => $row['ten_san_pham'],
             'tensanpham_slug' => Str::slug($row['ten_san_pham']),
             'baohanh' => $row['bao_hanh'],
@@ -29,9 +34,14 @@ class SanPhamImport implements ToModel, WithHeadingRow, WithValidation
     public function rules(): array
     {
         return [
-            'loai_san_pham' => 'required',
-            'hang_san_xuat' => 'required',
-            'noi_san_xuat' => 'required',
+            'hang_san_xuat'      => ['required'],
+            'noi_san_xuat'       => ['required'],
+            'loai_san_pham'      => ['required'],
+            'ten_san_pham'       => ['required'],
+            'bao_hanh'           => ['required'],
+            'so_luong'           => ['required'],
+            'don_gia'           => ['required'],
+            'thu_muc'            => ['required'],
         ];
     }
 }
